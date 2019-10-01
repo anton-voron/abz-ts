a1507b118b2ad1805dffd9fa06a19e7b6a3be6f2
anton-voron <anton.voron.gt@gmail.com>
2019-10-01 13:08:52 +0300

    section 4 finish + update style section 3

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,6 +7,7 @@ import Header from '../Header/Header'
 import Section1 from '../Section1/Section1'
 import Section2 from '../Section2/Section2'
 import Section3 from '../Section3/Section3'
+import Section4 from '../Section4/Section4'
 
 
 
@@ -18,6 +19,7 @@ function App () {
             <Section1 />
             <Section2 />
             <Section3 />
+            <Section4 />
         </main>
     )
 }
